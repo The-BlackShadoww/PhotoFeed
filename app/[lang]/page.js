425f9b0a6a06bde0bab7317af4e65d820db59dde
Ashikur Rahman <ashikur.jebu111@gmail.com>
2024-03-30 02:46:21 +0600

    project first draft

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,7 +1,12 @@
 import { getDictionary } from "./dictionaries";
+import PhotoList from "@/components/PhotoList";
 
 export default async function Home({ params: { lang } }) {
     const dictionary = await getDictionary(lang);
 
-    return <div>{dictionary.followers}</div>;
+    const response = await fetch(`${process.env.BASE_API_URL}/photos`);
+
+    const photos = await response.json();
+
+    return <PhotoList photos={photos} />;
 }
